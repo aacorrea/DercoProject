8727b93d58e56231cdb64c6fe14fddbc384259e8
Alejandro Correa <alejandromagallanes@gmail.com>
2022-02-17 14:06:18 -0300

    Updated

---
+++ model.py
@@ -0,0 +1,19 @@
+# https://www.section.io/engineering-education/deploying-machine-learning-models-using-flask/
+
+import pandas as pd  # To manage data as data frames
+import numpy as np  # To manipulate data as arrays
+import joblib
+from sklearn.linear_model import LogisticRegression  # Classification model
+
+# Dictionary containing the mapping
+cluster_mappings = {0: 'Segmento A', 1: 'Segmento B', 2: 'Segmento C'}
+
+logreg = joblib.load('cluster_model_derco.sav')
+
+# Function for classification based on inputs
+def classify(a, b, c, d, e, f, g):
+    arr = np.array([a, b, c, d, e, f, g])  # Convert to numpy array
+    arr = arr.astype(np.int)  # Change the data type to int
+    query = arr.reshape(1, -1)  # Reshape the array
+    prediction = cluster_mappings[logreg.predict(query)[0]]  # Retrieve from dictionary
+    return prediction  # Return the prediction
